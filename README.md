eb1932bb383b7908b62b5a02d748980e039decba
vaillan <red.line.v.o.s@outlook.com>
2023-08-07 00:11:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-gsguhs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gsguhs)
---
+++ index.js
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+
+//Variables globales
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+let numeroMayor = 0;
+
+//Procesos Generales
+document.getElementById('c').addEventListener('click', onInit);
+
+function onInit(e) {
+  num1 = document.getElementById('num1').value;
+  num2 = document.getElementById('num2').value;
+  num3 = document.getElementById('num3').value;
+  numeroMayor = Math.max(num1, num2, num3);
+  setAlert();
+}
+
+/**
+ * Función para mostrar etiqueta con el año, mes y días correspondientes
+ * @return {void}
+ */
+ function setAlert() {
+  let alert = document.getElementById('alert');
+  alert.removeAttribute('hidden');
+  alert.innerHTML = `
+  <p>EL número mayor es:
+    <span class="badge text-bg-success">${numeroMayor}</span>
+  </p>
+  `;
+}
